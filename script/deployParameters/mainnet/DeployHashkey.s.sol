d0fc48555f2bfdf500fd500d885ee2f218612532
Suncheol Kwon <elytselyts@gmail.com>
2025-02-26 21:18:25 +0900

    chore: deployed new contract

--- script/deployParameters/mainnet/DeployHashkey.s.sol
+++ script/deployParameters/mainnet/DeployHashkey.s.sol
@@ -17,7 +17,7 @@ import { RouterParameters } from '../../../src/base/RouterImmutables.sol';
 contract DeployHashkey is DeployUniversalRouter {
   /// @notice contract address will be based on deployment salt
   function getDeploymentSalt() public pure override returns (bytes32) {
-    return keccak256('HUK-V4-UNIVERSAL-ROUTER/UniversalRouter/0115-003');
+    return keccak256('HUK-V4-UNIVERSAL-ROUTER/UniversalRouter/0226-001');
   }
 
   // ref from v3 universal router: https://sepolia.etherscan.io/tx/0xb4610521d3fc61f4837edbd899acb6c33a5fe0f3bb32ab84745ac0a8b1859906
@@ -36,7 +36,7 @@ contract DeployHashkey is DeployUniversalRouter {
       v4Vault: UNSUPPORTED_PROTOCOL,
       v4ClPoolManager: UNSUPPORTED_PROTOCOL,
       v4BinPoolManager: UNSUPPORTED_PROTOCOL,
-      v3NFTPositionManager: 0x49A5B1A60dF367D40Df907E715Db1A54c67B3a2e,
+      v3NFTPositionManager: 0xE5396BDa286A0aFd178A64Bfb1704ad5E38375B9,
       v4ClPositionManager: UNSUPPORTED_PROTOCOL,
       v4BinPositionManager: UNSUPPORTED_PROTOCOL
     });
